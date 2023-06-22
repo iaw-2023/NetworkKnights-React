4b402220d1f6e842b366a8e92e34ff9eba90fdc8
MelinaDuran <melinaduran15@gmail.com>
2023-06-22 17:22:57 -0300

    Arreglo modal orden exitosa

--- src/app/adoptar/[id]/page.js
+++ src/app/adoptar/[id]/page.js
@@ -4,19 +4,29 @@ import { Form, FormGroup, FormControl, FormLabel, Button } from "react-bootstrap
 import Modal from 'react-bootstrap/Modal';
 import React, {useState, useEffect} from "react";
 import OrderModal from "../../components/OrderModal";
+import Link from "next/link";
 
 
 function AdoptarId({params}) {
+
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+
+  const handleClose = () => setShow(false);
 
 
   const apiURL = 'https://rodi-duran-laravel-79zb-hhxw14sen-network-knights.vercel.app/rest';
   const [email, setEmail] = useState('');
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();
+    
     const queryJson={
       email,
       id_pet: params.id
-    }
+    } 
 
     const response = await fetch(`${apiURL}/orders`, {
         method: 'POST',
@@ -25,11 +35,8 @@ function AdoptarId({params}) {
         },
         body: JSON.stringify(queryJson)
     });
-  };
 
- function mostrarModal() {
-    <OrderModal/>
-    
+   // handleClose();
   };
 
   return(
@@ -43,10 +50,33 @@ function AdoptarId({params}) {
         />
       </FormGroup>
     
-      <Button onClick={mostrarModal} variant="primary" type="submit">
-        Enviar orden
-       
+      <Button onClick={handleShow} variant="primary" type="submit">
+        Enviar ordennnnnnnnnn
       </Button>
+
+      <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+      >
+      <Modal show={show} onHide={handleClose}>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Orden Exitosa!</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Tu orden ha sido creada con éxito, nos comunicaremos por mail con los datos.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            <Link href={"/mascotas"} className='link-dark ms-2'>Aceptar</Link>
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+      </Modal>
+    </div>
+
     </Form>
   )
 }
