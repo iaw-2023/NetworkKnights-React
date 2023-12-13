a9b7f64f17399db369c9b25a28a9b6ddbfba099c
Melina Duran <41453257+MelinaDuran@users.noreply.github.com>
2023-12-12 21:53:07 -0300

    api img perros

--- next.config.js
+++ next.config.js
@@ -9,4 +9,4 @@ const nextConfig = {
     })
 }
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
--- src/app/components/HomeCarousel.js
+++ src/app/components/HomeCarousel.js
@@ -1,6 +1,20 @@
 import Carousel from 'react-bootstrap/Carousel';
+import React, {useState, useEffect} from "react";
 
 function HomeCarousel() {
+
+  const apiURL = "https://dog.ceo/api/breeds/image/random/4"
+  const [dogs, setDogs] = useState();
+
+  const getDogs = async() => {
+    const response = await fetch(apiURL);
+    setDogs(await response.json());
+  }
+
+  useEffect( () => {
+    getDogs();
+  },[]);
+
   return (
     <Carousel>
       <Carousel.Item>
@@ -12,19 +26,25 @@ function HomeCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://cdn.shopify.com/s/files/1/0468/5460/2913/files/certificado-de-adopcion-de-mascotas.jpg?v=1605174675"
+          src={!dogs ? "" : dogs.message[0]}
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://estaticos-cdn.elperiodico.com/clip/1aca279a-979d-4595-8a24-2411937a88e3_source-aspect-ratio_default_0.jpg"
+          src={!dogs ? "" : dogs.message[1]}
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://www.las2orillas.co/wp-content/uploads/2023/04/adoptar-mascota.jpg"
+          src={!dogs ? "" : dogs.message[2]}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={!dogs ? "" : dogs.message[3]}
         />
       </Carousel.Item>
       
