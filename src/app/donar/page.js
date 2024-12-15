f314180ba05dbae0b4f3892ab2c2fc88d4b0788c
Melina Duran <41453257+MelinaDuran@users.noreply.github.com>
2024-12-15 20:32:13 -0300

    Actualización MercadoPago
    Se agregó el código correspondiente con la configuración inicial del brick y también las llamadas correspondientes para poder comunicarse con el backend del proyecto y efectuar los pagos.

--- src/app/donar/page.js
+++ src/app/donar/page.js
@@ -10,8 +10,48 @@ import { initMercadoPago, CardPayment } from '@mercadopago/sdk-react';
 
 
 
+
 function donar() {
   initMercadoPago('TEST-6b3952a1-158a-46b6-8524-ddf57654222e');
+  
+    const initialization = {
+      amount: 100,
+    };
+    
+    const onSubmit = async (formData) => {
+      // callback llamado al hacer clic en el botón enviar datos
+      return new Promise((resolve, reject) => {
+        fetch('/process_payment', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            // recibir el resultado del pago
+            resolve();
+          })
+          .catch((error) => {
+            // manejar la respuesta de error al intentar crear el pago
+            reject();
+          });
+      });
+    };
+    
+    const onError = async (error) => {
+      // callback llamado para todos los casos de error de Brick
+      console.log(error);
+    };
+    
+    const onReady = async () => {
+      /*
+        Callback llamado cuando Brick está listo.
+        Aquí puedes ocultar cargamentos de su sitio, por ejemplo.
+      */
+    };
+ 
   return(
     <Container>
       <Row className="mt-4 ">
@@ -20,12 +60,21 @@ function donar() {
         </Col>
       </Row>
       <Row className="mt-4">
+      <h3>Doná $1000</h3>
         <Col xs={5} className="border rounded">
+        {/* 
           <CardPayment
-          initialization={{ amount: 100 }}
+          initialization={{ amount: 1000 }}
           onSubmit={async (param) => {
             console.log(param);
           }}
+          />
+        */}
+          <CardPayment
+             initialization={initialization}
+             onSubmit={onSubmit}
+             onReady={onReady}
+            onError={onError}
           />
         </Col>
         <Col xs={5} className=" mx-auto ">
