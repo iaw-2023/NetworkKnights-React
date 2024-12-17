d9e41e9dcdf74d90b7e18d7967a0379038ad431e
Melina Duran <41453257+MelinaDuran@users.noreply.github.com>
2024-12-17 18:11:06 -0300

    Cambio de Brick usado en MP
    Se utilizó el Wallet Brick para conectar con MercadoPago para poder hacer las donaciones

--- src/app/donar/page.js
+++ src/app/donar/page.js
@@ -2,86 +2,75 @@
 import { Row } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import DonationForm from "../components/DonationForm";
-import CardPaymentBrick from "../components/MercadoPago";
-import { initMercadoPago, CardPayment } from '@mercadopago/sdk-react';
-
-
-
-
-
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import { useState, useRef } from 'react';
+import Form from 'react-bootstrap/Form';
 
 function donar() {
-  initMercadoPago('TEST-6b3952a1-158a-46b6-8524-ddf57654222e');
-  
-    const initialization = {
-      amount: 100,
-    };
-    
-    const onSubmit = async (formData) => {
-      // callback llamado al hacer clic en el botón enviar datos
-      return new Promise((resolve, reject) => {
-        fetch('/process_payment', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        })
-          .then((response) => response.json())
-          .then((response) => {
-            // recibir el resultado del pago
-            resolve();
-          })
-          .catch((error) => {
-            // manejar la respuesta de error al intentar crear el pago
-            reject();
-          });
+  const apiURL = 'https://rodi-duran-laravel-79zb-h08vqyo8z-network-knights.vercel.app/rest';
+  const apiTest = 'http://127.0.0.1:8000/rest'
+  const [preferenciaId, setPreferenciaId] = useState(null);
+  const [montoDonacion, setMontoDonacion] = useState(null);
+  const monto = useRef(null);
+
+  initMercadoPago('TEST-6b3952a1-158a-46b6-8524-ddf57654222e', {
+    locale: "es-AR",
+  });
+
+  const crearPreferencia = async(monto) => {
+
+    try {
+        const response = await fetch(`${apiTest}/donacion`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "donacion",
+          quantity: 1,
+          amount: monto,
+        }),
       });
-    };
-    
-    const onError = async (error) => {
-      // callback llamado para todos los casos de error de Brick
+
+      const data = await response.json();
+      const { preference_id } = data;
+      return preference_id;
+
+    } catch(error) {
       console.log(error);
-    };
-    
-    const onReady = async () => {
-      /*
-        Callback llamado cuando Brick está listo.
-        Aquí puedes ocultar cargamentos de su sitio, por ejemplo.
-      */
-    };
- 
-  return(
-    <Container>
-      <Row className="mt-4 ">
-        <Col xs={10} className="mx-auto ">
-          <h1>Podes hacer tu donación a nuestra pagina web a través de MercadoPago </h1>
-        </Col>
-      </Row>
-      <Row className="mt-4">
-      <h3>Doná $1000</h3>
-        <Col xs={5} className="border rounded">
-        {/* 
-          <CardPayment
-          initialization={{ amount: 1000 }}
-          onSubmit={async (param) => {
-            console.log(param);
-          }}
-          />
-        */}
-          <CardPayment
-             initialization={initialization}
-             onSubmit={onSubmit}
-             onReady={onReady}
-            onError={onError}
-          />
-        </Col>
-        <Col xs={5} className=" mx-auto ">
-          <h3 className="border rounded"> Este año llevamos un total de donaciones de: </h3>
-        </Col>
-      </Row>
-    </Container>
-  )
-}
-export default donar;
+    }
+  }
+
+  const ejecutarDonacion = async (monto) => {
+    const id = await crearPreferencia(monto);
+    if(id) {
+      setPreferenciaId(id);
+    }
+  };
+
+    return(
+      <Container>
+        <Row className="mt-4 ">
+          <Col xs={10} className="mx-auto ">
+            <h1>Podes hacer tu donación a nuestra pagina web a través de MercadoPago </h1>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+        
+          <Col xs={5} className="border rounded">
+          <h3>Ingresá un monto:</h3>
+            <div class="input-group mb-3">
+              <span class="input-group-text">$</span>
+              <input type="text" class="form-control" onChange={(evento) => {ejecutarDonacion(evento.target.value)}}/>
+            </div>
+            <Wallet initialization={{preferenceId: preferenciaId, redirectMode: 'self'}}/>
+          </Col>
+          <Col xs={5} className=" mx-auto ">
+            <h3 className="border rounded"> Este año llevamos un total de donaciones de: </h3>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
+export default donar
